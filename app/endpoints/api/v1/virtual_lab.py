2483cf5d65589b6a8ee59f0e34a18103a4f7fb88
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2024-07-03 17:54:57 +0200

    Add reservation endpoint

--- app/endpoints/api/v1/virtual_lab.py
+++ app/endpoints/api/v1/virtual_lab.py
@@ -1,5 +1,7 @@
 """Virtual lab endpoints."""
 
+from uuid import UUID
+
 from fastapi import APIRouter
 from pydantic import UUID4
 
@@ -7,7 +9,7 @@ router = APIRouter()
 
 
 @router.get("/{virtual_lab_id}")
-async def get_virtual_lab(virtual_lab_id: UUID4) -> dict:
+async def get_virtual_lab(virtual_lab_id: UUID) -> dict:
     """Return the virtual lab."""
     return {
         "virtual_lab_id": virtual_lab_id,
