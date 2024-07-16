23dbf7fc3edaad36295a8427d5f37d4f07ca2634
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2024-07-16 10:41:04 +0200

    Rename base router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,9 +2,9 @@
 
 from fastapi import APIRouter
 
-from app.api import base, reservation, virtual_lab
+from app.api import reservation, root, virtual_lab
 
 router = APIRouter()
-router.include_router(base.router)
+router.include_router(root.router)
 router.include_router(reservation.router, prefix="/api/reservation", tags=["reservation"])
 router.include_router(virtual_lab.router, prefix="/api/virtual-lab", tags=["virtual-lab"])
