73e6b48f0d72aadf42d95145a74665b0f7609066
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2024-07-24 16:59:22 +0200

    Use usage_value instead of reserved_units and units

--- app/repository/account.py
+++ app/repository/account.py
@@ -7,7 +7,6 @@ from sqlalchemy import and_, true
 
 from app.constants import AccountType
 from app.db.model import Account
-from app.logger import L
 from app.repository.base import BaseRepository
 from app.schema.domain import Accounts, ProjAccount, RsvAccount, SysAccount, VlabAccount
 from app.utils import create_uuid
